e98f3a9b6a033518dc5db139f2743c2bc56a6066
Jean-Paul Lizotte <jean-paul.lizotte@emyode.com>
2024-01-07 12:32:09 -0500

    OMG removing armv7 too long to build

--- dockerjobs.ps1
+++ dockerjobs.ps1
@@ -3,4 +3,4 @@ docker buildx ./rm mybuilder
 docker buildx create --use --name mybuilder --driver-opt network=host --buildkitd-flags '--allow-insecure-entitlement network.host'
 docker buildx ls
 docker buildx inspect --bootstrap
-docker buildx build --platform=linux/arm64,linux/amd64,linux/arm/v7 -f dockerfile.new -t drjp81/nodered:dev --push --progress=plain .
+docker buildx build --platform=linux/arm64,linux/amd64 -f dockerfile.new -t drjp81/nodered:dev --push --progress=plain .
